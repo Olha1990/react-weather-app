90b93ec8848e50ebaad3b7c17d4fb058779023bb
Olha <olunchik.ivanova@gmail.com>
2022-12-13 20:09:27 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/olha-bondarenko-393090242/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Olha Bondarenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Olha1990/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/olha-bondarenko-393090242/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Olha Bondarenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Olha1990/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
